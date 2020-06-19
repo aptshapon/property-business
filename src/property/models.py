42c7d586204115aa40fed6270fd0324b906d3ef5
shsheikhbd <aptshapon@hotmail.com>
2020-06-20 03:12:37 +0600

    Created property app, views and templates

---
+++ src/property/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+
+# choices
+property_type = (
+    ('S', 'sale'),
+    ('R', 'rent'),
+)
+
+class Property(models.Model):
+    name = models.CharField(max_length=50)
+    property_type = models.CharField(choices=property_type, max_length=10)
+    category = models.ForeignKey('Category', null=True, on_delete=models.SET_NULL)
+    price = models.PositiveIntegerField()
+    area = models.PositiveIntegerField()
+    beds_number = models.PositiveIntegerField()
+    garages_number = models.PositiveIntegerField()
+    image = models.ImageField(upload_to='property', null=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = 'Properties'
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.category_name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
