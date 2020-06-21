16df361d12c790582cae91b667337a10e7d67212
shsheikhbd <aptshapon@hotmail.com>
2020-06-22 02:54:12 +0600

    Added home templates

--- src/property/models.py
+++ src/property/models.py
@@ -26,6 +26,7 @@ class Property(models.Model):
 
 class Category(models.Model):
     category_name = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='category', null=True)
 
     def __str__(self):
         return self.category_name
